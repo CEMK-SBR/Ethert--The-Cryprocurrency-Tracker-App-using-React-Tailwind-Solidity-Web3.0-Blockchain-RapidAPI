9da0f472d5b850a5ee75ad04b5157f78ab45a3b0
RITAM CHARAN <ritamcharan232001@gmail.com>
2022-01-16 19:55:50 +0530

    1st commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+function App() {
+  return (
+    <div className="">
+      <div className="gradient-bg-welcome  h-72"></div>
+      <div className="gradient-bg-services  h-72"></div>
+      <div className="gradient-bg-transactions  h-72"></div>
+      <div className="gradient-bg-footer  h-72"></div>
+    </div>
+  );
+}
+
+export default App;
